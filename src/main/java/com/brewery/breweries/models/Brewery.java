8b35d53151447eaa09cb797300960de0e292afef
jrsilveti <josersilveti27@gmail.com>
2022-02-14 18:03:39 -0500

    Added brewery object.

---
+++ src/main/java/com/brewery/breweries/models/Brewery.java
@@ -0,0 +1,24 @@
+package java.com.brewery.breweries.models;
+
+import java.util.Date;
+
+public class Brewery{
+    public String id;
+    public String name;
+    public String brewery_type;
+    public String street;
+    public String address_2;
+    public String address_3;
+    public String city;
+    public String state;
+    public String county_province;
+    public String postal_code;
+    public String country;
+    public String longitude;
+    public String latitude;
+    public String phone;
+    public String website_url;
+    public Date updated_at;
+    public Date created_at;
+}
+
