90c267b45d7f57976bdd77b1ecb94565222313af
jrsilveti <josersilveti27@gmail.com>
2022-02-14 19:39:50 -0500

    Updated controller to call service endpoint.

--- src/main/java/com/brewery/controllers/BreweryController.java
+++ src/main/java/com/brewery/controllers/BreweryController.java
@@ -1,20 +1,28 @@
 package com.brewery.controllers;
 
 import com.brewery.models.Brewery;
-import org.springframework.stereotype.Controller;
+import com.brewery.services.BreweryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/breweries")
 public class BreweryController {
 
+    @Autowired
+    BreweryService breweryService;
+
     @GetMapping("/list")
     public List<Brewery> getBreweryList() {
-        List<Brewery> breweryList = new ArrayList<>();
+        List<Brewery> breweryList;
+
+        breweryList = breweryService.getBreweryList();
+
+        //add error handling for this endpoint
 
         return breweryList;
     }
