1dbd5e0c554849477c85f2fa5d03febfc639976c
jrsilveti <josersilveti27@gmail.com>
2022-02-15 20:37:03 -0500

    Removed some unused imports.

--- src/main/java/com/brewery/controllers/BreweryController.java
+++ src/main/java/com/brewery/controllers/BreweryController.java
@@ -3,10 +3,11 @@ package com.brewery.controllers;
 import com.brewery.models.Brewery;
 import com.brewery.services.BreweryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,7 +26,19 @@ public class BreweryController {
     }
 
     @GetMapping("/breweries")
-    public List<Brewery> getBreweryByName(@RequestParam String name) {
+    public List<Brewery> getBreweries(@RequestParam String name) {
         return breweryService.getBrewery(name);
+    }
+
+    @GetMapping("/brewery")
+    public Brewery getBrewery(@RequestParam String name) {
+        List<Brewery> resultList = breweryService.getBrewery(name);
+        for(Brewery brewery: resultList) {
+            if(brewery.getName().equals(name)) {
+                return brewery;
+            }
+        }
+
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
     }
 }
